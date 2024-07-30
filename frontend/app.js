6dfbd4c935020f67f2a3a5dbc0923c2fc079e4ca
AzizbekEngineer <azizbekengineer@gmail.com>
2024-07-31 00:03:55 +0500

    add

--- frontend/app.js
+++ frontend/app.js
@@ -1,15 +1,12 @@
 const API_URL = "http://localhost:8000";
 const wrapper = document.querySelector(".wrapper");
 const form = document.querySelector(".form");
-
-const [
-  fnameInput,
-  lnameInput,
-  usernameInput,
-  passwordInput,
-  urlInput,
-  ageInput,
-] = form.querySelectorAll("input");
+const fnameInput = document.querySelector(".fname");
+const lnameInput = document.querySelector(".lname");
+const usernameInput = document.querySelector(".username");
+const passwordInput = document.querySelector(".password");
+const urlInput = document.querySelector(".url");
+const ageInput = document.querySelector(".age");
 
 async function fetchData(api) {
   let response = await fetch(`${api}/users`);
@@ -39,12 +36,13 @@ function userData(data) {
             <p><b>age</b>:${user.age}</p>
             <div class="btns">
                 <button>edit</button>
-                <button>delete</button>
+                <button class="delete__btn">delete</button>
             </div>
     `;
     wrapper.appendChild(card);
   });
 }
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   let newUser = {
@@ -53,6 +51,27 @@ form.addEventListener("submit", (e) => {
     username: usernameInput.value,
     password: passwordInput.value,
     url: urlInput.value,
-    age: ageInput.value,
+    age: +ageInput.value,
   };
+
+  fetch(`${API_URL}/users`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(newUser),
+  })
+    .then((res) => res.json())
+    .then((res) => fetchData(API_URL))
+    .catch((err) => console.log(err));
+});
+
+wrapper.addEventListener("click", (e) => {
+  if (e.target.className === "delete__btn") {
+    let id = e.target.closest(".card").dataset.id;
+    console.log(id);
+    fetch(`${API_URL}/users/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((res) => fetchData(API_URL));
+  }
 });
